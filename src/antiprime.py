9f0f32a92b7655f0ac39baf6bdd3373286a3bf67
Martina Rosales <martina.rosales01@estudiant.upf.edu>
2024-10-22 11:43:53 +0200

    third attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,10 +1,32 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(x) :
 
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	m = 0
+	h = 0
+	i = 1
+	t = int(x) - 1
+	while (i <= int(x)):
+		if (int(x) % i == 0):
+			m = m + 1
+		i = i + 1
+	while (t >= 1):
+		i = 1
+		while (i <= t):
+			if (t % i == 0):
+				h = h + 1
+			i = i + 1
+		if (h >= m):
+			return("not anti-prime")
+			t = 0
+		else:
+			t = t - 1
+			h = 0
+	if (h < m):
+		return("anti-prime")
 
 
 
@@ -19,6 +41,8 @@ def main() :
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys
+	x = int (sys.argv [1])
 
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
